fdddb52091ac1accfd50ea059d67b3118c5ba59d
Hiroyoshi HOUCHI <hixi@cpan.org>
2013-07-23 18:56:35 +0000

    Import of HIXI/Test-Stub-Generator-0.02 from CPAN.
    gitpan-cpan-distribution: Test-Stub-Generator
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         HIXI/Test-Stub-Generator-0.02.tar.gz
    gitpan-cpan-author:       HIXI
    gitpan-cpan-maturity:     released

--- README.md
+++ README.md
@@ -3,13 +3,14 @@
 Test::Stub::Generator - be able to generate stub (submodule and method) having check argument and control return value.
 
 # SYNOPSIS
+
     use strict;
     use warnings;
 
     use Test::More;
     use Test::Deep;
     use Test::Deep::Matcher;
-    use Test::Stub::Generator;
+    use Test::Stub::Generator qw(make_method_utils);
 
     ###
     # sample package
@@ -25,48 +26,44 @@ Test::Stub::Generator - be able to generate stub (submodule and method) having c
 
     my $MEANINGLESS = -1;
 
-    my ($method, $util) = make_method_utils(
+    my ($stub_method, $util) = make_method_utils(
     #my $method = make_method(
         [
-            # automatic checking method arguments
+            # checking argument
             { expects => [ 0, 1 ], return => $MEANINGLESS },
             # control return_values
             { expects => [$MEANINGLESS], return => [ 0, 1 ] },
 
             # expects supported ignore(Test::Deep) and type(Test::Deep::Matcher)
-            { expects => [ignore, 1], return => $MEANINGLESS },
+            { expects => [ignore, 1],  return => $MEANINGLESS },
             { expects => [is_integer], return => $MEANINGLESS },
         ],
-        {
-            display => 'synopisis'
-        }
+        { message => 'method arguments are ok' }
     );
 
-    *Some::Class::method = $method;
     my $obj = Some::Class->new;
+    *Some::Class::method = $stub_method;
+    # ( or use Test::Mock::Guard )
+    # my $mock_guard = mock_guard( $obj => +{ method => $stub_method } );
 
     # { expects => [ 0, 1 ], return => xxxx }
     $obj->method( 0, 1 );
-    # ok xxxx- [synopisis] arguments are as You expected
-    # ( automaic checking method arguments )
+    # ok xxxx- method arguments are ok
 
-    # { expects => xxxx, return => [ 0, 1 ] }
     is_deeply( $obj->method($MEANINGLESS), [ 0, 1 ], 'return values are as You expected' );
+    # { expects => xxxx, return => [ 0, 1 ] }
     # ok xxxx- return values are as You expected
-    # ( control method return_value )
 
-    # { expects => [ignore, 1], return => xxxx }
     $obj->method( sub{}, 1 );
-    # ok xxxx- [synopisis] arguments are as You expected
-    # ( automatic checking to exclude ignore fields )
+    # { expects => [ignore, 1], return => xxxx }
+    # ok xxxx- method arguments are ok
 
-    # { expects => [is_integer], return => xxxx }
     $obj->method(1);
-    # ok xxxx- [synopisis] arguments are as You expected
-    # ( automatic checking to use Test::Deep::Matcher )
+    # { expects => [is_integer], return => xxxx }
+    # ok xxxx- method arguments are ok
 
-    ok(!$util->has_next, 'empty');
-    is $util->called_count, 4, 'called_count is 4';
+    ok( !$util->has_next, 'empty' );
+    is( $util->called_count, 4, 'called_count is 4' );
 
     done_testing;
 
@@ -76,71 +73,76 @@ Test::Stub::Generator is library for supports the programmer in wriring test cod
 
 # Functions
 
-## make\_subroutine($expects\_and\_return\_list, $opts)
+## make\_subroutine($expects\_and\_return, $opts)
 
 simulate subroutine (do not receive $self)
 
-## make\_method($expects\_and\_return\_list, $opts)
+## make\_method($expects\_and\_return, $opts)
 
 simulate object method (receive $self)
 
 # Parameters
 
-## $expects\_and\_return\_list(first arguments)
+## $expects\_and\_return(first arguments)
 
+$expects\_and\_return required the hash\_ref (single or array\_ref)
+
+    my $method = make_method(
+        { expects => [1], return => 2 }
+    );
     my $method = make_method(
       [
         { expects => [1], return => 2 }
+        { expects => [2], return => 3 }
       ]
     );
 
 - expects
 
-automaic checking $method\_argument
+    automaic checking $method\_argument
 
-    $method->(1); # ok xxxx- [stub] arguments are as You expected
+        $method->(1); # ok xxxx- [stub] arguments are as You expected
 
 - return
 
-control return\_value
+    control return\_value
 
-    my $return = $method->(1); # $return = 2;
+        my $return = $method->(1); # $return = 2;
 
 ## $opts(second arguments)
 
     my $method = make_method(
-      [
-        { expects => [1], return => 2 }
-      ],
-      { display => "display name", is_repeat => 1 }
+      { expects => [1], return => 2 },
+      { message => "arguments are ok", is_repeat => 1 }
     );
 
-- display
+- message
 
-change displayed name when testing
+    change message
 
 - is\_repeat
 
-repeat mode ( repeating $expects\_and\_return\_list->{0\] )
+    repeat mode ( repeating $expects\_and\_return->{0\] )
+    ( can use make\_repeat\_method / make\_repeat\_subroutine )
 
 # Utility Method (second return\_value method)
 
-    my ($method, $util) = make_subroutine_utils($expects_and_return_list, $opts)
-    my ($method, $util) = make_method_utils($expects_and_return_list, $opts)
+    my ($method, $util) = make_subroutine_utils($expects_and_return, $opts)
+    my ($method, $util) = make_method_utils($expects_and_return, $opts)
 
 - $util->called\_count
 
-return a number of times that was method called
+    return a number of times that was method called
 
 - $util->has\_next
 
-return a boolean.
-if there are still more $expects\_and\_return\_list, then true(1).
-if there are not, then false(0).
+    return a boolean.
+    if there are still more $expects\_and\_return\_list, then true(1).
+    if there are not, then false(0).
 
 - $util->is\_repeat
 
-return a value $opt->{is\_repeat}
+    return a value $opt->{is\_repeat}
 
 # Setting Sheat
 
--- lib/Test/Stub/Generator.pm
+++ lib/Test/Stub/Generator.pm
@@ -9,97 +9,62 @@ use Test::Deep;
 use Exporter qw(import);
 use Carp qw(croak);
 use Class::Monadic;
+use Data::Util qw(is_array_ref is_code_ref);
 
-our $VERSION   = "0.01";
-our @EXPORT    = qw(make_subroutine make_subroutine_utils make_method make_method_utils);
-our @EXPORT_OK = qw(make_subroutine_from_list make_method_from_list);
+our $VERSION   = "0.02";
+our @EXPORT    = qw(make_subroutine make_method);
+our @EXPORT_OK = qw(
+    make_subroutine_utils
+    make_method_utils
+    make_repeat_subroutine
+    make_repeat_method
+);
 
 sub make_subroutine {
-    my ($ers, $opts) = @_;
-    $opts ||= {};
-    return scalar _build_subroutine( $ers, { %{$opts}, is_object => 0 } );
+    my (@args)  = @_;
+    return _convert_build( [@args], is_object => 0 );
 }
 
 sub make_subroutine_utils {
-    my ($ers, $opts) = @_;
-    $opts ||= {};
-    return _build_subroutine( $ers, { %{$opts}, is_object => 0 } );
+    my (@args)  = @_;
+    return _convert_build( [@args], is_object => 0, want_utils => 1 );
+}
+
+sub make_repeat_subroutine {
+    my (@args)  = @_;
+    return _convert_build( [@args], is_object => 0, is_repeat => 1 );
 }
 
 sub make_method {
-    my ($ers, $opts) = @_;
-    $opts ||= {};
-    return scalar _build_subroutine( $ers, { %{$opts}, is_object => 1 } );
+    my (@args)  = @_;
+    return _convert_build( [@args], is_object => 1 );
 }
 
 sub make_method_utils {
-    my ($ers, $opts) = @_;
+    my (@args)  = @_;
+    return _convert_build( [@args], is_object => 1, want_utils => 1 );
+}
+
+sub make_repeat_method {
+    my (@args)  = @_;
+    return _convert_build( [@args], is_object => 1, is_repeat => 1 );
+}
+
+sub _convert_build {
+    my ($args, %inner_opts) = @_;
+    my ($exp_ret_list, $opts) = @$args;
+    $exp_ret_list = [$exp_ret_list] unless is_array_ref $exp_ret_list;
     $opts ||= {};
-    return _build_subroutine( $ers, { %{$opts}, is_object => 1 } );
+    return _build( $exp_ret_list, { %{ $opts }, %inner_opts } );
 }
 
-sub make_subroutine_from_list {
-    my %args = @_;
-    my $opts = $args{opts} || {};
-    return scalar _from_list(
-        $args{expects},
-        $args{return},
-        { %{ $opts }, is_object => 0 },
-    );
-}
+sub _build {
+    my ($exp_ret_list, $opts) = @_;
 
-sub make_subroutine_utils_from_list {
-    my %args = @_;
-    my $opts = $args{opts} || {};
-    return _from_list(
-        $args{expects},
-        $args{return},
-        { %{ $opts }, is_object => 0 },
-    );
-}
-
-sub make_method_from_list {
-    my %args = @_;
-    my $opts = $args{opts} || {};
-    return scalar _from_list(
-        $args{expects},
-        $args{return},
-        { %{ $opts }, is_object => 1 },
-    );
-}
-
-sub make_method_utils_from_list {
-    my %args = @_;
-    my $opts = $args{opts} || {};
-    return _from_list(
-        $args{expects},
-        $args{return},
-        { %{ $opts }, is_object => 1 },
-    );
-}
-
-sub _from_list {
-    my ( $expects, $return, $opts ) = @_;
-    if( scalar @{$expects} != scalar @{$return} ) {
-        croak "expects size and return size are different";
-    }
-    my $er_list = [];
-    for (my $i = 0; $i < scalar @$expects; $i++){
-        push @$er_list, {
-            expects => $expects->[$i],
-            return  => $return->[$i],
-        };
-    }
-
-    return _build_subroutine( $er_list, $opts );
-}
-
-sub _build_subroutine {
-    my ($er_list, $opts) = @_;
-
-    my $display    = $opts->{display}   || 'stub';
-    my $is_object  = $opts->{is_object} || 0;
-    my $is_repeat  = $opts->{is_repeat} || 0;
+    my $message    = $opts->{message}    || "[stub] arguments are as You expected";
+    my $is_object  = $opts->{is_object}  || 0;
+    my $is_repeat  = $opts->{is_repeat}  || 0;
+    my $want_utils = $opts->{want_utils} || 0;
     my $call_count = 0;
 
     my $method = sub {
@@ -107,29 +72,29 @@ sub _build_subroutine {
         shift @$input if $is_object;
         $call_count++;
 
-        my $er = $is_repeat? $er_list->[0] : shift @$er_list;
-        unless ( defined $er ) {
+        my $element = $is_repeat ? $exp_ret_list->[0] : shift @$exp_ret_list;
+        unless ( defined $element ) {
             fail 'expects and return are already empty.';
             return undef;
         }
 
-        my $expects = $er->{expects};
-        my $return  = $er->{return};
+        my $expects = $element->{expects};
+        my $return  = $element->{return};
 
-        cmp_deeply($input, $expects, "[$display] arguments are as You expected")
+        cmp_deeply($input, $expects, $message)
             or note explain +{ input => $input, expects => $expects };
 
-        return (ref $return eq 'CODE')? $return->() : $return;
+        return is_code_ref($return) ? $return->() : $return;
     };
 
-    if (!wantarray) {
+    unless ($want_utils) {
         return $method;
     }
 
-    my $util = bless( {}, 'Test::Stub::Generator::Util' );
-    Class::Monadic->initialize($util)->add_methods(
+    my $utils = bless( {}, 'Test::Stub::Generator::Util' );
+    Class::Monadic->initialize($utils)->add_methods(
         has_next => sub {
-            return @$er_list ? 1 : 0;
+            return @$exp_ret_list ? 1 : 0;
         },
         is_repeat => sub {
             return $is_repeat;
@@ -139,7 +104,7 @@ sub _build_subroutine {
         },
     );
 
-    return ($method, $util);
+    return ($method, $utils);
 }
 
 1;
@@ -152,13 +117,14 @@ __END__
 Test::Stub::Generator - be able to generate stub (submodule and method) having check argument and control return value.
 
 =head1 SYNOPSIS
+
     use strict;
     use warnings;
 
     use Test::More;
     use Test::Deep;
     use Test::Deep::Matcher;
-    use Test::Stub::Generator;
+    use Test::Stub::Generator qw(make_method_utils);
 
     ###
     # sample package
@@ -174,48 +140,44 @@ Test::Stub::Generator - be able to generate stub (submodule and method) having c
 
     my $MEANINGLESS = -1;
 
-    my ($method, $util) = make_method_utils(
+    my ($stub_method, $util) = make_method_utils(
     #my $method = make_method(
         [
-            # automatic checking method arguments
+            # checking argument
             { expects => [ 0, 1 ], return => $MEANINGLESS },
             # control return_values
             { expects => [$MEANINGLESS], return => [ 0, 1 ] },
 
             # expects supported ignore(Test::Deep) and type(Test::Deep::Matcher)
-            { expects => [ignore, 1], return => $MEANINGLESS },
+            { expects => [ignore, 1],  return => $MEANINGLESS },
             { expects => [is_integer], return => $MEANINGLESS },
         ],
-        {
-            display => 'synopisis'
-        }
+        { message => 'method arguments are ok' }
     );
 
-    *Some::Class::method = $method;
     my $obj = Some::Class->new;
+    *Some::Class::method = $stub_method;
+    # ( or use Test::Mock::Guard )
+    # my $mock_guard = mock_guard( $obj => +{ method => $stub_method } );
 
     # { expects => [ 0, 1 ], return => xxxx }
     $obj->method( 0, 1 );
-    # ok xxxx- [synopisis] arguments are as You expected
-    # ( automaic checking method arguments )
+    # ok xxxx- method arguments are ok
 
-    # { expects => xxxx, return => [ 0, 1 ] }
     is_deeply( $obj->method($MEANINGLESS), [ 0, 1 ], 'return values are as You expected' );
+    # { expects => xxxx, return => [ 0, 1 ] }
     # ok xxxx- return values are as You expected
-    # ( control method return_value )
 
-    # { expects => [ignore, 1], return => xxxx }
     $obj->method( sub{}, 1 );
-    # ok xxxx- [synopisis] arguments are as You expected
-    # ( automatic checking to exclude ignore fields )
+    # { expects => [ignore, 1], return => xxxx }
+    # ok xxxx- method arguments are ok
 
-    # { expects => [is_integer], return => xxxx }
     $obj->method(1);
-    # ok xxxx- [synopisis] arguments are as You expected
-    # ( automatic checking to use Test::Deep::Matcher )
+    # { expects => [is_integer], return => xxxx }
+    # ok xxxx- method arguments are ok
 
-    ok(!$util->has_next, 'empty');
-    is $util->called_count, 4, 'called_count is 4';
+    ok( !$util->has_next, 'empty' );
+    is( $util->called_count, 4, 'called_count is 4' );
 
     done_testing;
 
@@ -225,23 +187,31 @@ Test::Stub::Generator is library for supports the programmer in wriring test cod
 
 =head1 Functions
 
-=head2 make_subroutine($expects_and_return_list, $opts)
+=head2 make_subroutine($expects_and_return, $opts)
 
 simulate subroutine (do not receive $self)
 
-=head2 make_method($expects_and_return_list, $opts)
+=head2 make_method($expects_and_return, $opts)
 
 simulate object method (receive $self)
 
 =head1 Parameters
 
-=head2 $expects_and_return_list(first arguments)
+=head2 $expects_and_return(first arguments)
 
+$expects_and_return required the hash_ref (single or array_ref)
+
+  my $method = make_method(
+      { expects => [1], return => 2 }
+  );
   my $method = make_method(
     [
       { expects => [1], return => 2 }
+      { expects => [2], return => 3 }
     ]
   );
+
+=over
 
 =item expects
 
@@ -255,27 +225,34 @@ control return_value
 
     my $return = $method->(1); # $return = 2;
 
+=back
+
 =head2 $opts(second arguments)
 
   my $method = make_method(
-    [
-      { expects => [1], return => 2 }
-    ],
-    { display => "display name", is_repeat => 1 }
+    { expects => [1], return => 2 },
+    { message => "arguments are ok", is_repeat => 1 }
   );
 
-=item display
+=over
 
-change displayed name when testing
+=item message
+
+change message
 
 =item is_repeat
 
-repeat mode ( repeating $expects_and_return_list->{0] )
+repeat mode ( repeating $expects_and_return->{0] )
+( can use make_repeat_method / make_repeat_subroutine )
+
+=back
 
 =head1 Utility Method (second return_value method)
 
-  my ($method, $util) = make_subroutine_utils($expects_and_return_list, $opts)
-  my ($method, $util) = make_method_utils($expects_and_return_list, $opts)
+  my ($method, $util) = make_subroutine_utils($expects_and_return, $opts)
+  my ($method, $util) = make_method_utils($expects_and_return, $opts)
+
+=over
 
 =item $util->called_count
 
@@ -290,6 +267,8 @@ if there are not, then false(0).
 =item $util->is_repeat
 
 return a value $opt->{is_repeat}
+
+=back
 
 =head1 Setting Sheat
 
